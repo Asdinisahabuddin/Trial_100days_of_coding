83119b12d16106503852f2645834e6d09b80abdb
Asdinisahabuddin <147237150+Asdinisahabuddin@users.noreply.github.com>
2023-10-10 14:51:35 +0700

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,24 @@
+package Trial_100days_of_coding;
+
+public class day2 {
+    public static void main(String[] args) {
+        byte a = 127;
+        short b = 32767;
+        int c = 2134567898;
+        long d = 9223372036854775805L;
+        float e =678.34f;
+        double f =675.954321;
+        char g = 'A';
+        boolean h =true;
+        
+        System.out.println("syte   :"+a);
+        System.out.println("short  :"+b);
+        System.out.println("int    :"+c);
+        System.out.println("long   :"+d);
+        System.out.println("float  :"+e);
+        System.out.println("double :"+f);
+        System.out.println("char   :"+g);
+        System.out.println("boolean:"+h);
+    }
+    
+}
